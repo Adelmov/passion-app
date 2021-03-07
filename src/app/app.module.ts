7e16bbd66344d1cfe5ed0d9168ce3f53ac079626
TXT-15 <txt-15@TXT-15s-MBP.lan>
2021-03-06 20:19:45 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule} from '@angular/common'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { AboutMeAComponent } from './about-me/about-me-a/about-me-a.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    OurStoryComponent,
+    AboutMeAComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
